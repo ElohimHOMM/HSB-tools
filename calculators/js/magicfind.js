2b01f59692c382d03de1eec123b6a5a0ab4118da
Elohim <robertf.seitz@gmail.com>
2025-07-18 09:29:40 +0200

    Add Magic Find Calculator (without Dropdowns for Mobs)

---
+++ calculators/js/magicfind.js
@@ -0,0 +1,14 @@
+$(function() {
+    includeNavbar();
+    $("#start-button").click(function() {
+        let baseDropChance = $("#input-bdc").val();
+        let magicFind = $("#input-mf").val();
+        let output = $("#p-output");
+
+        let chances = (baseDropChance / 100) * (1 + (magicFind / 100))
+        let inAmount = Math.round((1 / chances) * 100) / 100;
+        chances = 100 * chances;
+
+        output.html(`Chances: ${chances}%<br>That is a 1 in ${inAmount}`)
+    });
+});
---
+++ common/navbar.js
@@ -0,0 +1,41 @@
+function includeNavbar() {
+  $("#navbar-container").html(`<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/index.html">HSB Tools</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Calculators
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="/calculators/magicfind.html">Magic Find</a></li>
+                  <!--
+                  <li><a class="dropdown-item" href="/2015/day02.html">Day 02</a></li>
+                  <li><a class="dropdown-item" href="/2015/day03.html">Day 03</a></li>
+                  <li><a class="dropdown-item" href="/2015/day04.html">Day 04</a></li>
+                  <li><a class="dropdown-item" href="/2015/day05.html">Day 05</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Day 11</a></li>
+                  -->           
+                </ul>
+              </li>
+              <!--
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Unrelated
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="/unrelated/mox.html">Mox' Hobby Page</a></li>
+                </ul>
+              </li>
+              -->
+            </ul>
+          </div>
+        </div>
+    </nav>`);
+}
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
