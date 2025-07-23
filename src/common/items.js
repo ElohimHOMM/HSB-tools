f08c5919eb530a2cd2b8874f956f6b76dd5c51ea
Elohim <robertf.seitz@gmail.com>
2025-07-23 16:10:07 +0200

    update items path

--- src/common/items.js
+++ src/common/items.js
@@ -1,6 +1,6 @@
 function readItems() {    
     let items = $.ajax({
-        url: "/common/items.json",
+        url: "/src/common/items.json",
         dataType: 'json',
         async: false
     }).responseJSON.items;
